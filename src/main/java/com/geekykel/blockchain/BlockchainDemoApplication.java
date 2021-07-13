eb68b9f20ba1ef44e4fb438f630a978f104aff79
Kelvin Isievwore <kelvinator4leo@gmail.com>
2021-07-13 13:28:22 +0100

    InitializingBean to Save User Roles On Startup

--- src/main/java/com/geekykel/blockchain/BlockchainDemoApplication.java
+++ src/main/java/com/geekykel/blockchain/BlockchainDemoApplication.java
@@ -1,7 +1,13 @@
 package com.geekykel.blockchain;
 
+import com.geekykel.blockchain.model.ERole;
+import com.geekykel.blockchain.model.Role;
+import com.geekykel.blockchain.repository.RoleRepository;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Created by Kelvin on 01/07/2021.
@@ -11,6 +17,19 @@ public class BlockchainDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BlockchainDemoApplication.class, args);
+    }
+
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Bean
+    InitializingBean saveUserRoles() {
+        return () -> {
+            roleRepository.save(new Role(ERole.ROLE_USER));
+            roleRepository.save(new Role(ERole.ROLE_ADMIN));
+            roleRepository.save(new Role(ERole.ROLE_MODERATOR));
+        };
     }
 
 }
