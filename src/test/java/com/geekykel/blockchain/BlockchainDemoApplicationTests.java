ddeb52223fe1b00d9e398d4c428f081c94a1b289
Kelvin Isievwore <kelvinator4leo@gmail.com>
2021-07-20 00:00:48 +0100

    Added Sample Unit Test

--- src/test/java/com/geekykel/blockchain/BlockchainDemoApplicationTests.java
+++ src/test/java/com/geekykel/blockchain/BlockchainDemoApplicationTests.java
@@ -1,13 +1,23 @@
 package com.geekykel.blockchain;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class BlockchainDemoApplicationTests {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BlockchainDemoApplicationTests.class);
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void githubAction() {
+        LOG.info("Success");
+        Assertions.assertEquals(5 + 2, 7);
+    }
 }
